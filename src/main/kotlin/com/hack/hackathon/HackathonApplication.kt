a7c591f8a0964c6d3b1c8a3a73deaea1d270ae33
IS1DI <vyacheslav.ivankov@aisa.ru>
2024-04-20 12:35:01 +0300

    restTemplate

--- src/main/kotlin/com/hack/hackathon/HackathonApplication.kt
+++ src/main/kotlin/com/hack/hackathon/HackathonApplication.kt
@@ -2,6 +2,8 @@ package com.hack.hackathon
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
 class HackathonApplication
@@ -9,3 +11,7 @@ class HackathonApplication
 fun main(args: Array<String>) {
     runApplication<HackathonApplication>(*args)
 }
+
+@Bean
+fun restTemplate() : RestTemplate =
+    RestTemplate()
