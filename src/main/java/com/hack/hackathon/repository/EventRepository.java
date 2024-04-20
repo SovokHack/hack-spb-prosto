8ef4ca85a2889eba5716dc9c90b2f355f1bb3ff0
garrow <garik.steam.11@yandex.ru>
2024-04-20 12:39:36 +0300

    Add Event Tools

--- src/main/java/com/hack/hackathon/repository/EventRepository.java
+++ src/main/java/com/hack/hackathon/repository/EventRepository.java
@@ -1,8 +1,8 @@
 package com.hack.hackathon.repository;
 
 import com.hack.hackathon.entity.Event;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository
-        extends CrudRepository<Event, Long> {
+        extends JpaRepository<Event, Long> {
 }
--- src/main/java/com/hack/hackathon/service/EventService.java
+++ src/main/java/com/hack/hackathon/service/EventService.java
@@ -1,20 +1,74 @@
 package com.hack.hackathon.service;
 
 import com.hack.hackathon.entity.Event;
-import kotlin.jvm.functions.Function1;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.hack.hackathon.exception.Exceptions;
+import com.hack.hackathon.exception.NotFoundException;
+import com.hack.hackathon.repository.EventRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 
-public class EventService extends AbstractCrudService<Event> {
-    @NotNull
-    @Override
-    public Function1<Long, RuntimeException> supplierNotFound() {
-        return null;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Slf4j
+public class EventService {
+    private final EventRepository eventRepository;
+
+    private final ModelMapper modelMapper;
+
+    public Event getById(Long id)
+            throws NotFoundException {
+        Optional<Event> foundCharacter = eventRepository.findById(id);
+
+        return foundCharacter.orElseThrow(() -> new NotFoundException(Exceptions.EVENT_NOT_FOUND));
     }
 
-    @NotNull
-    @Override
-    public JpaRepository getRepository() {
-        return null;
+    public void existsById(Long id)
+            throws NotFoundException {
+        eventRepository.findById(id).orElseThrow(() -> new NotFoundException(Exceptions.EVENT_NOT_FOUND));
+    }
+
+    public Page<Event> getAll(Pageable pageable){
+        return eventRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public void saveAll(List<Event> eventList){
+        eventRepository.saveAll(eventList);
+    }
+
+    @Transactional
+    public Event create(Event event) {
+        eventRepository.save(event);
+
+        log.info("Event with ID {} is created", event);
+
+        return event;
+    }
+
+    @Transactional
+    public Event update(Event unsavedEvent, Long id) {
+        Event event = new Event();
+        modelMapper.map(getById(id), event);
+        modelMapper.map(unsavedEvent, event);
+        eventRepository.save(event);
+
+        log.info("Event with ID {} is updated", event.getId());
+
+        return event;
+    }
+
+    @Transactional
+    public void delete(Long id)
+            throws NotFoundException {
+        existsById(id);
+        eventRepository.deleteById(id);
+
+        log.info("Event with ID {} is deleted", id);
     }
 }
