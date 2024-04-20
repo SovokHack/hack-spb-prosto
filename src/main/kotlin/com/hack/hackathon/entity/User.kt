74dce33c3dcce9d03bc8b1d79f8e90e730c51a1c
Сергей Рудов <rudov.200317@gmail.com>
2024-04-20 21:32:47 +0300

    externalservice

--- src/main/kotlin/com/hack/hackathon/entity/User.kt
+++ src/main/kotlin/com/hack/hackathon/entity/User.kt
@@ -2,18 +2,26 @@ package com.hack.hackathon.entity
 
 import com.hack.hackathon.security.Role
 import jakarta.persistence.*
-import org.hibernate.proxy.HibernateProxy
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
 
 @Entity(name = "users")
 @Table(name = "users")
-data class User (
-    var role : Role,
-    @Column(name = "group_")
-    var group : String,
-    var password : String,
-    var username : String,
-    var homeAddress : String,
-    @OneToMany(mappedBy = "user") var events : MutableList<Event> = mutableListOf(),
+
+data class User(
+    var role: Role,
+    @NotNull(message = "Значение не должно быть пустым")
+    @Column(name = "group_") var group: String,
+    @NotNull(message = "Значение не должно быть пустым") @Size(
+        min = 5,
+        max = 15,
+        message = "Длина пароля должна быть не менее 5 символов и не более 15 символов!"
+    ) var password: String,
+    @Size(
+        min = 5, max = 15, message = "Длина имени пользователя должна быть не менее 5 символов и не более 15 символов!"
+    ) @NotNull(message = "Значение не должно быть пустым")
+    var username: String,
+    @NotNull(message = "Значение не должно быть пустым")
+    var homeAddress: String,
     @Id @GeneratedValue var id: Long? = null
-) {
-}
+)
