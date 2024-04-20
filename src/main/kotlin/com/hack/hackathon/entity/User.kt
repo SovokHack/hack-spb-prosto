1f42b9f5b62684b017d8337b1abcdfe808597a66
IS1DI <vyacheslav.ivankov@aisa.ru>
2024-04-20 15:38:05 +0300

    security

--- src/main/kotlin/com/hack/hackathon/entity/User.kt
+++ src/main/kotlin/com/hack/hackathon/entity/User.kt
@@ -4,14 +4,17 @@ import com.hack.hackathon.security.Role
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import org.hibernate.proxy.HibernateProxy
 
-@Entity
+@Entity(name = "users")
+@Table(name = "users")
 data class User (
     var role : Role,
     var group : String,
     var password : String,
     var username : String,
+    var homeAddress : String,
     @Id @GeneratedValue var id: Long? = null
 ) {
 }
