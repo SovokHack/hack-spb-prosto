754b12dc4ec37b9a88edab32721807c8e9d88b31
Иванков Вячеслав Валерьевич <vyacheslav.ivankov@aisa.ru>
2024-04-21 16:15:30 +0300

    user view coordinates

--- src/main/kotlin/com/hack/hackathon/view/MainView.kt
+++ src/main/kotlin/com/hack/hackathon/view/MainView.kt
@@ -1,6 +1,7 @@
 package com.hack.hackathon.view
 
 import com.hack.hackathon.layout.MainLayout
+import com.hack.hackathon.service.RoutingService
 import com.vaadin.flow.component.html.Div
 import com.vaadin.flow.component.html.Span
 import com.vaadin.flow.router.PageTitle
@@ -11,8 +12,12 @@ import jakarta.annotation.security.PermitAll
 @PageTitle("App | Main")
 @Route("mainView", layout = MainLayout::class)
 @AnonymousAllowed
-class MainView : Div() {
+class MainView(
+    private val routingService: RoutingService
+) : Div() {
     init {
+        val r = routingService.route(34.45435,24.6546,54.654,23.45)
         add(Span("Hello World!"))
+
     }
 }
